1ab8151a514401db15050c7b413b6643deefd11b
Vladislav Zablocki <zablvm27@gmail.com>
2017-02-13 14:41:55 +0300

    task_DEV-2 calculator

---
+++ Task_DEV-2/Calculator.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_DEV_2
+{
+    // Class which gets checed string and do all operation 
+    class Calculator
+    {
+        private string inputString;
+        private string expressionOperator = "*-+/";
+
+        // constructor 
+        public Calculator(string inputString)
+        {
+            this.inputString = inputString;
+        }
+
+        // method which make oeration with 2 variables
+        // and return result
+        private double[] ResultOfOperation(double[] stack, ref bool[] isNumber,
+            int stackCount, string operation)
+        {
+            double firstVariable = 0;
+            double secondVariable = 0;
+            int i = 0;
+            while (i < stackCount)
+            {
+                i++;
+                if (isNumber[stackCount - i])
+                {
+                    secondVariable = stack[stackCount - i];
+                    isNumber[stackCount - i] = false;
+                    stack[stackCount - i] = 0;
+                    while (i < stackCount)
+                    {
+                        i++;
+                        if (isNumber[stackCount - i])
+                        {
+                            firstVariable = stack[stackCount - i];
+                            isNumber[stackCount - i] = false;
+                            stack[stackCount - i] = 0;
+                            break;
+                        }
+                    }
+                    isNumber[stackCount] = true;
+                    break;
+                }
+
+            }
+            switch (operation)
+            {
+                case "*":
+                    stack[stackCount] = firstVariable * secondVariable;
+                    return stack;
+                case "/":
+                    stack[stackCount] = firstVariable / secondVariable;
+                    return stack;
+                case "+":
+                    stack[stackCount] = firstVariable + secondVariable;
+                    return stack;
+                case "-":
+                    stack[stackCount] = firstVariable - secondVariable;
+                    return stack;
+                default:
+                    return stack;
+            }
+
+        }
+
+        // method which gets string in postfix format and do all operation
+        // and return result
+        public double Calculate()
+        {
+            Converter converter = new Converter(inputString);
+            bool isOperator = false;
+            string[] outputString = converter.ConvertingFromInfixToPostfix();
+            double[] stack = new double[converter.GetQuantityOfOperation()];
+            bool[] isNumber = new bool[converter.GetQuantityOfOperation()];
+            int stackCount = 0;
+            string operation=null;
+
+            foreach (var item in outputString)
+            {
+                if (item == null)
+                {
+                    break;
+                }
+                foreach (var oper in expressionOperator)
+                {
+                    if (item == oper.ToString())
+                    {
+                        isOperator = true;
+                        operation = oper.ToString();
+                        break;
+                    }
+                }
+                if (isOperator == false && item!=null)
+                {
+                    isNumber[stackCount] = true;
+                    stack[stackCount] = Double.Parse(item);
+                }
+                else
+                {
+                    stack = ResultOfOperation(stack,ref isNumber, stackCount, operation);
+                    isOperator = false;
+                }
+                stackCount++;
+            }
+            return stack[stackCount-1];
+        } 
+    }
+}
---
+++ Task_DEV-2/Converter.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_DEV_2
+{
+    // Class which gets string from "Checker" and convert it 
+    // in postfix format
+    class Converter
+    {
+        private string inputString;
+        private string expressionOperator = "*-+/()";
+
+        // concstructor 
+        public Converter(string inputString)
+        {
+            this.inputString = inputString;
+        }
+
+        // splittig string to array of string
+        // in this array elements is operator or variable
+        private string[] SplittingString()
+        {
+            string[] changedString = new string[GetQuantityOfOperation()];
+            string cell = null;
+            bool isOperator = false;
+            int newCount = 0;
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                foreach (var oper in expressionOperator)
+                {
+                    if (inputString[i] == oper)
+                    {
+                        isOperator = true;
+                        break;
+                    }
+                }
+                if (isOperator == false)
+                {
+                    cell = String.Concat(cell, inputString[i]);
+                    if (inputString[i] == 'E' || inputString[i] == 'e')
+                    {
+                        i++;
+                        cell = String.Concat(cell, inputString[i]);
+                    }
+                }
+                else
+                {
+                    if (i == 0)
+                    {
+                        changedString[newCount] = inputString[i].ToString();
+                    }
+                    else
+                    {
+                        if (cell == null)
+                        {
+                            changedString[newCount] = inputString[i].ToString();
+
+                        }
+                        else
+                        {
+                            changedString[newCount] = cell;
+                            newCount++;
+                            changedString[newCount] = inputString[i].ToString();
+                            cell = null;
+                        }
+                    }
+                    newCount++;
+                    isOperator = false;
+                }
+            }
+            changedString[newCount] = cell;
+            return changedString;
+        }
+
+        // return priority of operatoin
+        private int GetPriority(string expressionOperator)
+        {
+            switch (expressionOperator)
+            {
+                case "*":
+                    return 2;
+                case "/":
+                    return 2;
+                case "+":
+                    return 1;
+                case "-":
+                    return 1;
+                default:
+                    return 0;
+            }
+        }
+
+        // calculate numbers of operation to know
+        // size array of string 
+        public int GetQuantityOfOperation()
+        {
+            int count=0;
+            foreach(var item in inputString)
+            {
+                foreach (var oper in expressionOperator)
+                {
+                    if (item == oper)
+                    {
+                        count++;
+                        break;
+                    }
+                }
+                if (item == 'e' || item == 'E')
+                {
+                    count--;
+                }
+            }
+            return 2*count+1;
+        }
+
+        // converting expression from infix to postfix form 
+        // it's done to make calculations easier
+        public string[] ConvertingFromInfixToPostfix()
+        {
+            string[] stack = new string[GetQuantityOfOperation()];
+            string[] outputString = new string[GetQuantityOfOperation()];
+            bool isOperator = false;
+            int stackCount = 0;
+            int outputCount = 0;
+
+            foreach (var items in SplittingString())
+            {
+                if (items == null)
+                    break;
+                foreach (var oper in expressionOperator)
+                {
+                    if (items == oper.ToString())
+                    {
+                        isOperator = true;
+                        break;
+                    }
+                }
+                if (isOperator == false)
+                {
+                    outputString[outputCount] = items;
+                    outputCount++;
+                }
+                else 
+                {
+                    if (items == ")")
+                    {
+                        while (stack[stackCount-1] != "(")
+                        {
+                            outputString[outputCount] = stack[stackCount - 1];
+                            stackCount--;
+                            stack[stackCount] = null;
+                            outputCount++;
+                        }
+                        stackCount--;
+                        stack[stackCount] = null;
+                        
+                    }
+                    else
+                    {
+                        isOperator = false;
+                        if (stackCount == 0 || items=="(")
+                        {
+                            stack[stackCount] = items;
+                            stackCount++;
+                        }
+                        else
+                        {
+                            if (GetPriority(items) >= GetPriority(stack[stackCount - 1]))
+                            {
+                                stack[stackCount] = items;
+                                stackCount++;
+                            }
+                            else
+                            {
+                                while (stackCount > 0)
+                                {
+                                    outputString[outputCount] = stack[stackCount - 1];
+                                    stackCount--;
+                                    stack[stackCount] = null;
+                                    outputCount++;
+                                }
+                                stack[stackCount] = items;
+                                stackCount++;
+                            }
+                        }
+                    }
+                }
+            }
+            while (stackCount > 0)
+            {
+                outputString[outputCount] = stack[stackCount - 1];
+                stackCount--;
+                stack[stackCount] = null;
+                outputCount++;
+            }
+            return outputString;
+        }
+    }
+}
