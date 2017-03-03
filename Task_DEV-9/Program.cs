40418d013806c0bb0217e32a98a67f22dacff430
Vladislav Zablocki <zablvm27@gmail.com>
2017-03-03 10:03:46 +0300

    DEV-9 find values in file by section and key

---
+++ Task_DEV-9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace task_DEV_9
+{
+    class Program
+    {
+        /// <summary>
+        /// Entry point of programm.Input section and key and find all values
+        /// </summary>
+        /// <param name="args">Path to the file</param>
+        static void Main(string[] args)
+        {
+            Inputer inputer = new Inputer();
+            Finder finder = new Finder();
+            string section = inputer.InputSection();
+            string key = inputer.InputKey();
+            try
+            {
+                List<string> result = finder.FindValuesInFile(section, key, args[0]);
+                foreach (var item in result)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Error! Add path to the file");
+            }
+        }
+
+    }
+}
